5fed0ae260e5b20f6b706b00ae7c4ad0bf6db123
LexKora <kargin.alexej2010@yandex.ru>
2022-08-31 16:58:46 +0300

    graphic field2
    нарисовано графическое поле

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,14 @@
 # попытка номер 3
 empty = '-'
-FILD = {}
+FIELD = {}
 for i in range(1,4):
     for j in range(1,4):
-        FILD [int(str(i)+str(j))] = empty
-
+        FIELD [int(str(i)+str(j))] = empty
+def graphic_field(kvarg):
+    if kvarg == None :
+        kvarg = {}
+    print(f'   1  2  3')
+    print(f' 1 {FIELD.get(11)}  {FIELD.get(12)}  {FIELD.get(13)}')
+    print(f' 2 {FIELD.get(21)}  {FIELD.get(22)}  {FIELD.get(23)}')
+    print(f' 3 {FIELD.get(31)}  {FIELD.get(32)}  {FIELD.get(33)}')
+graphic_field(FIELD)

8ebf6cd5cfe7fb0227d0eaba32a26ff7a324dd98
LexKora <kargin.alexej2010@yandex.ru>
2022-08-31 17:43:52 +0300

    move
    добавил прорисовку крестика на заданном месте

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,14 +1,23 @@
-# попытка номер 3
+# попытка номер 4
 empty = '-'
+cross = 'X'
+zero = 'O'
 FIELD = {}
 for i in range(1,4):
     for j in range(1,4):
         FIELD [int(str(i)+str(j))] = empty
-def graphic_field(kvarg):
-    if kvarg == None :
-        kvarg = {}
+def graphic_field():
     print(f'   1  2  3')
     print(f' 1 {FIELD.get(11)}  {FIELD.get(12)}  {FIELD.get(13)}')
     print(f' 2 {FIELD.get(21)}  {FIELD.get(22)}  {FIELD.get(23)}')
     print(f' 3 {FIELD.get(31)}  {FIELD.get(32)}  {FIELD.get(33)}')
-graphic_field(FIELD)
+def you_move():
+     move = 0
+     while not (move in FIELD):
+        move = int(input('Введите Ваш ход : '))
+     FIELD [move] = cross
+#def computer_move():
+    
+graphic_field()
+you_move()
+graphic_field()
